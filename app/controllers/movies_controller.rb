f3938baed81df20030514e54ee47ad0b676a6bca
Hoa Dam <hoangocdam@gmail.com>
2024-03-20 13:31:47 -0700

    feat:show method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -33,5 +33,15 @@ class MoviesController < ApplicationController
     @top_rated_movies = data[:results].first(20)
   end
 
+  def show
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['Authorization'] =
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYmNkNTNjOTBjMjE3NmVmYTg3MDY3NGM2N2NjNjAxNSIsInN1YiI6IjY1ZjkwY2FkMzg0NjlhMDE0OTdkNTI0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P64TF2h_KOK6_bJc8o777vt23F71GxnoiAPCosoTj34'
+    end
 
+    response = conn.get("/3/movie/#{params[:id]}?append_to_response=reviews%2Ccredits")
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    @movie = data
+  end
 end
