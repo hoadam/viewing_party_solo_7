7972f8cb108d4c682feb0e5daa99de33f5000601
Hoa Dam <hoangocdam@gmail.com>
2024-03-20 13:20:42 -0700

    feat:test for show page

---
+++ spec/features/discovers/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Movies Page', type: :feature do
+  describe 'When user visits "/discover"' do
+    before(:each) do
+      @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
+
+      visit user_discover_path(@user.id)
+    end
+
+    it 'has two buttons to search for top rated movies and for the movies that have the keywords' do
+      expect(page).to have_link('Find Top Rated Movies')
+      expect(page).to have_field('q')
+      expect(page).to have_button('Find Movies')
+    end
+  end
+end
