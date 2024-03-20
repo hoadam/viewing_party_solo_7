a8782b2ccceaa78404789873abf7c14ddff5a2e9
Hoa Dam <hoangocdam@gmail.com>
2024-03-20 13:14:21 -0700

    create gem weblock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -70,4 +70,12 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<API_KEY>') { Rails.application.credentials.tmdb[:key] }
+  config.default_cassette_options = { re_record_interval: 14.days }
+  config.configure_rspec_metadata!
 end
