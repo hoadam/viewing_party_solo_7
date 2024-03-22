a548c306b9f5677efb122a66e83ae728f138d342
Hoa Dam <hoangocdam@gmail.com>
2024-03-21 21:53:06 -0700

    feat:feature test story4

---
+++ spec/features/viewing_parties/new_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'The New Viewing Party Page', type: :feature do
+  describe 'When I visit the new viewing party page' do
+    before(:each) do
+      @user1 = User.create!(name: 'Tommy', email: 'tommy@email.com')
+      @user2 = User.create!(name: 'Jerry', email: 'jerry@email.com')
+      @user3 = User.create!(name: 'Elizabeth', email: 'elizabeth@email.com')
+    end
+
+    it "shows the form to create new viewing party", :vcr => { cassette_name: 'tmdb_movies' } do
+      visit new_user_movie_viewing_party_path(@user1.id, "155")
+
+      fill_in "Duration of Party", with: 152
+      fill_in "Day", with: "03/22/24"
+      fill_in "Start Time", with: "20:00"
+      fill_in "Guest 1 Email", with: "jerry@email.com"
+      fill_in "Guest 2 Email", with: "elizabeth@email.com"
+
+      click_on "Create Party"
+      save_and_open_page
+      expect(current_path).to eq(user_path(@user1.id))
+      expect(page).to have_content("Successfully Created New Viewing Party")
+      expect(page).to have_content("Movie Title: The Dark Knight")
+      expect(page).to have_content("Party Time: 03/22/24 at 20:00")
+      expect(page).to have_content("Tommy")
+      expect(page).to have_content("Jerry")
+      expect(page).to have_content("Elizabeth")
+
+      save_and_open_page
+    end
+  end
+end
