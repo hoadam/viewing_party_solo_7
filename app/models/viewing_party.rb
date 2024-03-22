79ef91581336859c44630244bb2d9efb51db7895
Hoa Dam <hoangocdam@gmail.com>
2024-03-21 21:55:46 -0700

    feat:validates

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,7 +2,13 @@ class ViewingParty < ApplicationRecord
    has_many :user_parties
    has_many :users, through: :user_parties
 
+   validates :duration, presence: true, numericality: {only_integer: true}
+   validates :date, presence: true, format: { with: /\A\d{2}\/\d{2}\/\d{2}\z/, message: "should be a MM/DD/YY format"}
+   validates :start_time, presence: true, format: { with: /\A\d{2}:\d{2}\z/, message: "should be a HH/MM format"}
+
    def find_host
       users.where("user_parties.host = true").first
    end
+
+
 end
