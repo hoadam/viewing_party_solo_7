e13892241f78399a374aaf9eb7825d0060c2cbd6
Hoa Dam <hoangocdam@gmail.com>
2024-04-01 16:58:34 -0700

    fix:create method and user_params to require password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,59 @@
 class UsersController < ApplicationController
-   def new
-      @user = User.new
-   end
-
-   def show
-      @user = User.find(params[:id])
-      movie_service = MovieService.new
-      @image_base_url = movie_service.get_image_base_url
-
-      @movie_info = {}
-      @user.viewing_parties.each do |party|
-         if party.movie_id != nil
-
-            movie = movie_service.movie_details(party.movie_id)
-
-            @movie_info[party.id] = {
-               title: movie.title,
-               poster: @image_base_url + movie.poster
-            }
-         end
-      end
-   end
-
-   def create
-      user = User.new(user_params)
-      if user.save
-         flash[:success] = 'Successfully Created New User'
-         redirect_to user_path(user)
-      else
-         flash[:error] = "#{error_message(user.errors)}"
-         redirect_to register_user_path
-      end
-   end
-
-private
-
-  def user_params
-      params.require(:user).permit(:name, :email)
+  def new
+    @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    movie_service = MovieService.new
+    @image_base_url = movie_service.get_image_base_url
+
+    @movie_info = {}
+    @user.viewing_parties.each do |party|
+      next if party.movie_id.nil?
+
+      movie = movie_service.movie_details(party.movie_id)
+
+      @movie_info[party.id] = {
+        title: movie.title,
+        poster: @image_base_url + movie.poster
+      }
+    end
+  end
+
+  def create
+    user_params[:email] = user_params[:email].downcase
+    user = User.new(user_params)
+    #  if user.password == user.password_confirmation
+    if user.save
+      flash[:success] = 'Successfully Created New User'
+      redirect_to user_path(user)
+    else
+      flash[:error] = "#{error_message(user.errors)}"
+      redirect_to register_user_path
+    end
+    #  else
+    #    flash[:error] = 'Password and password confirmation do not match'
+    #    redirect_to register_user_path
+    #  end
+  end
+
+  def login_form; end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.name}"
+      redirect_to user_path(user)
+    else
+      flash[:error] = 'Sorry, your credentials are bad.'
+      render :login_form
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
