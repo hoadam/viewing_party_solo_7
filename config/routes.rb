4006afc8d98e8ed8c079479b41247d18eb76d1b0
Hoa Dam <hoangocdam@gmail.com>
2024-04-01 17:09:16 -0700

    create:routes for login and login_form

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get '/register', to: 'users#new', as: 'register_user'
-
-  resources :users, only: %i[show create] do
-    # get '/discover', to: 'users#discover', as: 'discover'
+  get '/login', to: 'users#login_form', as: 'user_login'
+  post '/login', to: 'users#login'
+  resources :users, only: %i[show new create] do
     resource :discover, only: %i[show]
     resources :movies, only: %i[index show] do
       resource :similar, only: %i[show]
